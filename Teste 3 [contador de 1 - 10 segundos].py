193decbd3b3926593224954800ef101f579b2d94
Ruben Saraiva <120272892+Ruben-Saraiva@users.noreply.github.com>
2022-12-14 10:12:53 -0300

    Add files via upload
    Código de elaboração básica referente a um contador simples de 1 a 10, utilizando delay de 1 segundo na linguagem Python.

---
+++ Teste 3 [contador de 1 - 10 segundos].py
@@ -0,0 +1,5 @@
+#contador simples de 10 segundos
+import time
+for cont in range(1, 11): #contador crescente de 1 - 10
+    print(cont)
+    time.sleep(1) #determina o tempo de espera para o seguir da contagem
